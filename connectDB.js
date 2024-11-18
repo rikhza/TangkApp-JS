44d74df138a98b676f13eaa8a7205962f2df193c
Annas Ismail <86274500+AnnasIsmail@users.noreply.github.com>
2024-11-18 09:52:33 +0700

    Update connectDB.js

--- connectDB.js
+++ connectDB.js
@@ -3,6 +3,6 @@ const db = process.env.DB;
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    "mongodb://127.0.0.1:27017/tangkApp",
-    // "mongodb+srv://annas:annas123@cluster0.ey2dmu0.mongodb.net/tangkApp"
+    // "mongodb://127.0.0.1:27017/tangkApp",
+    "mongodb+srv://annas:annas123@cluster0.ey2dmu0.mongodb.net/tangkApp"
 );
