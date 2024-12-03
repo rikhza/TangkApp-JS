5d1e2f1093862d1257740f54443c2cbef9c01848
Muhammad Rikhza (Riza) Maulana <muhammad.maulana026@binus.ac.id>
2024-12-03 11:53:43 +0700

    update riza

--- connectDB.js
+++ connectDB.js
@@ -1,8 +1,7 @@
-
 const db = process.env.DB;
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    // "mongodb://127.0.0.1:27017/tangkApp",
-    "mongodb+srv://annas:annas123@cluster0.ey2dmu0.mongodb.net/tangkApp"
+	// "mongodb://127.0.0.1:27017/tangkApp",
+	"mongodb+srv://cigico:fakechar11@cigi.1v9ma.mongodb.net/tangkapp"
 );
